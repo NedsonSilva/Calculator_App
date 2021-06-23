7f59d69c76ee6079a597a15d9a041d8307c6a188
Nedson <nedsonsa@gmail.com>
2021-06-23 16:41:53 -0300

    Reorganizing files, code optimization

--- js/main.js
+++ js/main.js
@@ -13,26 +13,27 @@ const calc = {
    },
    
    formatExpression() {
-      const InvalidPointer = /[+\-x/](?=\.)/g
+      const expression = result.innerText
       const regex = /\d\.\d+(\.)/g
+      const InvalidPointer = /[+\-x/](?=\.)/g
       const regexPointer = /\.(?=\.)/g
       const operatorInvalid = /[+\-x/](?=\/|\+|-|x)/g
+      const pointerInOperator = /\.(?=[+\-x/])/g
 
       function updateExpression() {
          setTimeout(() => 
-            result.innerText = result.innerText.substring(0, result.innerText.length - 1)
+            result.innerText = result.innerText
+               .substring(0, result.innerText.length - 1)
          , 100)
       }
 
-      if(regex.test(result.innerText)) updateExpression()
+      const verification = 
+         regex.test(expression) || regexPointer.test(expression)
+         || result.innerText.includes('//') || operatorInvalid.test(expression)
+         || pointerInOperator.test(expression) 
+         || result.innerText.lastIndexOf('/') === 0
       
-      else if(regexPointer.test(result.innerText)) updateExpression()
-
-      if(result.innerText.includes('//')) updateExpression()
-
-      if(result.innerText.lastIndexOf('/') === 0) updateExpression()
-
-      if(operatorInvalid.test(result.innerText)) updateExpression()
+      if(verification) updateExpression()
 
       if(result.innerText.search(/\./) === 0)
          result.innerText = result.innerText.replace('.', '0.')
@@ -82,27 +83,20 @@ const calc = {
    }
 }
 
-for(let operation of getOperation)
-   operation.addEventListener('click', calc.get)
+getOperation.forEach( get => get.addEventListener('click', calc.get))
 
 //Alter Theme
 const theme = document.querySelector('.alter-theme')
-let cont = 2;
 
 function alter() {
-   
    const body = document.body
 
-   if(cont === 2) 
+   if(!body.hasAttribute('class'))
       body.classList.add('theme2')
-   else if(cont === 3) 
+   else if(body.classList.contains('theme2'))
       body.classList.replace('theme2', 'theme3')
-   else 
-      body.classList.remove('theme3')
-      
-   cont++
-
-   if(cont > 3) cont = 1
+   else
+      body.removeAttribute('class')
 }
 
 theme.addEventListener('click', alter)
