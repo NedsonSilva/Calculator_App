2986f680925791a6d6048f3f6d73b53ea4a14173
Nedson <nedsonsa@gmail.com>
2021-07-16 10:15:03 -0300

    function alter theme correction

--- js/main.js
+++ js/main.js
@@ -1,17 +1,14 @@
 const result = document.querySelector('#result p')
 const getOperation = document.querySelectorAll('.list-btn span')
 
-const calc = { 
+const calc = {
    get() {
       result.classList.remove('error')
-      
       result.innerText += this.innerText.replace('*', 'x')
-      
       calc.formatExpression()
-
       calc.resizeFont()
    },
-   
+
    formatExpression() {
       const expression = result.innerText
       const regex = /\d\.\d+(\.)/g
@@ -21,31 +18,27 @@ const calc = {
       const pointerInOperator = /\.(?=[+\-x/])/g
 
       function updateExpression() {
-         setTimeout(() => 
+         setTimeout(() =>
             result.innerText = result.innerText
-               .substring(0, result.innerText.length - 1)
-         , 100)
+            .substring(0, result.innerText.length - 1), 100)
       }
 
-      const verification = 
-         regex.test(expression) || regexPointer.test(expression)
-         || result.innerText.includes('//') || operatorInvalid.test(expression)
-         || pointerInOperator.test(expression) 
-         || result.innerText.lastIndexOf('/') === 0
-      
-      if(verification) updateExpression()
+      const verification = regex.test(expression) ||
+         regexPointer.test(expression) ||
+         result.innerText.includes('//') ||
+         operatorInvalid.test(expression) ||
+         pointerInOperator.test(expression) ||
+         result.innerText.lastIndexOf('/') === 0
 
-      if(result.innerText.search(/\./) === 0)
-         result.innerText = result.innerText.replace('.', '0.')
+      if (verification) updateExpression()
+      if (result.innerText.search(/\./) === 0) result.innerText = result.innerText.replace('.', '0.')
 
       result.innerText = result.innerText.replace(InvalidPointer, '$&0')
    },
 
    delete() {
       const back = result.innerText
-
       result.innerText = back.substring(0, back.length - 1)
-
       calc.resizeFont()
    },
 
@@ -55,17 +48,16 @@ const calc = {
    },
 
    calculation() {
-      if(result.innerText.search(/,/g) === 1)
+      if (result.innerText.search(/,/g) === 1)
          result.innerText = result.innerText.replace(/,/g, '.')
 
       const expression = result.innerText.replace(/x/g, '*')
 
       try {
-         if(Number.isInteger(eval(expression)))
+         if (Number.isInteger(eval(expression)))
             result.innerText = eval(expression)
          else
             result.innerText = eval(expression).toFixed(2).replace(/\./, ',')
-   
       } catch (e) {
          result.classList.add('error')
       } finally {
@@ -75,28 +67,29 @@ const calc = {
 
    resizeFont() {
       const textLength = result.innerText.length
-
-      if(textLength > 12)
-         result.style.fontSize = '1.5rem'
-      else 
-         result.style.fontSize = '2.2rem'
+      if (textLength > 12) result.style.fontSize = '1.5rem'
+      else result.style.fontSize = '2.2rem'
    }
 }
 
-getOperation.forEach( get => get.addEventListener('click', calc.get))
+getOperation.forEach(get => get.addEventListener('click', calc.get))
 
 //Alter Theme
 const theme = document.querySelector('.alter-theme')
 
-function alter() {
+let themeAlt = 1
+
+function alterTheme() {
    const body = document.body
 
-   if(!body.hasAttribute('class'))
+   if (themeAlt === 1)
       body.classList.add('theme2')
-   else if(body.classList.contains('theme2'))
+   else if (themeAlt === 2)
       body.classList.replace('theme2', 'theme3')
    else
-      body.removeAttribute('class')
+      body.classList.remove('theme2', 'theme3')
+
+   themeAlt > 2 ? themeAlt = 1 : themeAlt++
 }
 
-theme.addEventListener('click', alter)
+theme.addEventListener('click', alterTheme)
