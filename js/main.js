5a5ca5632c254ae47b2babe052248d0c20e23a44
Nedson <nedsonsa@gmail.com>
2022-06-04 20:10:00 -0300

    correção de bugs. Add preferência de tema ao localStorage

--- js/main.js
+++ js/main.js
@@ -10,10 +10,10 @@ const calc = {
    },
 
    formatExpression() {
-      const expression = result.innerText
+      const expression = String(result.innerText)
       const regex = /\d\.\d+(\.)/g
       const InvalidPointer = /[+\-x/](?=\.)/g
-      const regexPointer = /\.(?=\.)/g
+      const hasPointerDupliqued = /\.(?=\.)/g
       const operatorInvalid = /[+\-x/](?=\/|\+|-|x)/g
       const pointerInOperator = /\.(?=[+\-x/])/g
 
@@ -24,11 +24,13 @@ const calc = {
       }
 
       const verification = regex.test(expression) ||
-         regexPointer.test(expression) ||
-         result.innerText.includes('//') ||
+         hasPointerDupliqued.test(expression) ||
+         expression.includes('//') ||
          operatorInvalid.test(expression) ||
-         pointerInOperator.test(expression) ||
-         result.innerText.lastIndexOf('/') === 0
+         pointerInOperator.test(expression)
+
+      
+      if (expression.startsWith('/') || expression.startsWith('x')) updateExpression()
 
       if (verification) updateExpression()
       if (result.innerText.search(/\./) === 0) result.innerText = result.innerText.replace('.', '0.')
@@ -75,21 +77,31 @@ const calc = {
 getOperation.forEach(get => get.addEventListener('click', calc.get))
 
 //Alter Theme
-const theme = document.querySelector('.alter-theme')
 
-let themeAlt = 1
+let themeAlt = Number.parseInt(localStorage.getItem('theme')) || 2
+setTheme()
 
 function alterTheme() {
+   themeAlt > 3 ? themeAlt = 2 : themeAlt++
+
+   setTheme()
+
+   localStorage.setItem('theme', JSON.stringify(themeAlt))
+}
+
+function setTheme() {
    const body = document.body
 
-   if (themeAlt === 1)
+   if (themeAlt === 2) 
       body.classList.add('theme2')
-   else if (themeAlt === 2)
-      body.classList.replace('theme2', 'theme3')
+   else if (themeAlt === 3) {
+      body.classList.add('theme3')
+      body.classList.remove('theme2')
+   }
    else
-      body.classList.remove('theme2', 'theme3')
-
-   themeAlt > 2 ? themeAlt = 1 : themeAlt++
+      body.classList.remove('theme3', 'theme2')
 }
+
+const theme = document.querySelector('.alter-theme')
 
 theme.addEventListener('click', alterTheme)
